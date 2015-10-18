8628e4498d13e7d9231a28a8760bee08c48e79f6
mesudarshan7 <mesudarshan7@gmail.com>
2015-10-18 10:47:09 -1000

    named Queries at ENTITY

--- src/org/hibernate/crud/UserCrud.java
+++ src/org/hibernate/crud/UserCrud.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQuery(name="userByName",query="from UserCrud where id>?")
 public class UserCrud {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
